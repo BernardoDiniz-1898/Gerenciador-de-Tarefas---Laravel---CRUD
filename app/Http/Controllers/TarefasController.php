8b9ee327d587d4ffac3729fb843fda0684e93c48
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 <BernardoDiniz-1898>
2026-03-30 21:35:55 -0300

    controller tarefas feito e valdado

---
+++ app/Http/Controllers/TarefasController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tarefa;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class TarefasController extends Controller
+{
+    public function index()
+    {
+        $tarefas = Tarefa::latest('created_at')->get();
+        return view('tarefas.index', compact('tarefas'));
+    }
+
+    public function create()
+    {
+        return view('tarefas.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'nome' => ['required', 'string', 'min:3', 'max:255'],
+            'descricao' => ['nullable', 'string'],
+            'status' => ['required', 'string', Rule::in(['concluida', 'em andamento', 'incompleta'])],
+            'data_finalizacao' => ['nullable', 'date'],
+        ]);
+
+        Tarefa::create($validated);
+
+        return redirect()->route('tarefas.index');
+    }
+
+    public function edit(Tarefa $tarefa)
+    {
+        return view('tarefas.edit', compact('tarefa'));
+    }
+
+    public function update(Request $request, Tarefa $tarefa)
+    {
+        $validated = $request->validate([
+            'nome' => ['required', 'string', 'min:3', 'max:255'],
+            'descricao' => ['nullable', 'string'],
+            'status' => ['required', 'string', Rule::in(['concluida', 'em andamento', 'incompleta'])],
+            'data_finalizacao' => ['nullable', 'date'],
+        ]);
+
+        $tarefa->update($validated);
+        return redirect->route('tarefas.index');
+    }
+
+    public function destroy(Tarefa $tarefa)
+    {
+        $tarefa->delete();
+        return redirect()->route('tarefas.index');
+    }
+}
